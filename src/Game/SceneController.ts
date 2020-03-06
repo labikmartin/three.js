a990e1c51f328428359036b5e73d137946ed3262
Labik Martin <labikm@cngroup.dk>
2020-03-06 13:45:43 +0100

    Orbit Controls

--- src/Game/SceneController.ts
+++ src/Game/SceneController.ts
@@ -1,11 +1,15 @@
 import * as THREE from 'three';
 
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
 export default class SceneController {
   scene = new THREE.Scene();
   renderer = new THREE.WebGLRenderer({ antialias: true });
   camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
 
   sceneEl = this.renderer.domElement;
+
+  controls = new OrbitControls(this.camera, this.sceneEl);
 
   shapes: Record<string, unknown> = {};
 
@@ -20,6 +24,12 @@ export default class SceneController {
 
   setDefaultSettings() {
     this.camera.position.z = 5;
+
+    this.controls.screenSpacePanning = true;
+    this.controls.minDistance = 5;
+    this.controls.maxDistance = 40;
+    this.controls.target.set(0, 0, 0);
+    this.controls.update();
 
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     this.renderer.setClearColor('#dedede');
@@ -41,34 +51,29 @@ export default class SceneController {
   createSphereScene() {
     const mainSphereObject = {
       geometry: new THREE.SphereGeometry(2, 15, 15),
-      material: new THREE.MeshBasicMaterial({ color: 0xff0000, vertexColors: true }),
+      material: new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true }),
       get mesh() {
         return new THREE.Mesh(this.geometry, this.material);
       },
     };
-
     this.shapes.mainSphereObject = mainSphereObject;
 
-    this.scene.add(mainSphereObject.mesh);
+    var ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+    var pointLight = new THREE.PointLight(0xffffff, 1);
+
+    this.scene.add(mainSphereObject.mesh, ambientLight);
+    this.camera.add(pointLight);
+
+    const controlsAnimation = () => {
+      requestAnimationFrame(controlsAnimation);
+      this.controls.update();
+      this.renderer.render(this.scene, this.camera);
+    };
+
+    controlsAnimation();
   }
 
   attachEventListeners() {
     window.addEventListener('resize', () => this.handleResize());
-
-    this.sceneEl.addEventListener(
-      'mousewheel',
-      (event: Event) => {
-        const e = event as MouseWheelEvent;
-
-        e.preventDefault();
-
-        this.camera.position.z = this.camera.position.z + (e.deltaY > 0 ? 0.05 : -0.05);
-
-        this.reRender();
-
-        return true;
-      },
-      false,
-    );
   }
 }
